ccf78dd332482c2227c803bf128c83891fe64974
mirage335 <spamfreemirage335@gmail.com>
2015-11-14 22:42:13 -0500

    Laser Driver schematic, accurate footprints and BOM data.

---
+++ LaserDriver.cmd
@@ -0,0 +1,87 @@
+# Pin name action command file
+
+# Start of element D1
+ChangePinName(D1, 1, 1)
+ChangePinName(D1, 2, 2)
+
+# Start of element D3
+ChangePinName(D3, 1, 1)
+ChangePinName(D3, 2, 2)
+
+# Start of element D2
+ChangePinName(D2, 1, 1)
+ChangePinName(D2, 2, 2)
+
+# Start of element F1
+ChangePinName(F1, 2, 2)
+ChangePinName(F1, 1, 1)
+
+# Start of element D4
+ChangePinName(D4, 1, 1)
+ChangePinName(D4, 2, 2)
+
+# Start of element L2
+ChangePinName(L2, 1, 1)
+ChangePinName(L2, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, -)
+ChangePinName(C2, 1, +)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element LASER1
+ChangePinName(LASER1, 9, 9)
+ChangePinName(LASER1, 8, 8)
+ChangePinName(LASER1, 7, 7)
+ChangePinName(LASER1, 6, 6)
+ChangePinName(LASER1, 5, 5)
+ChangePinName(LASER1, 4, 4)
+ChangePinName(LASER1, 3, 3)
+ChangePinName(LASER1, 2, 2)
+ChangePinName(LASER1, 1, 1)
+
+# Start of element IF1
+ChangePinName(IF1, 9, 9)
+ChangePinName(IF1, 8, 8)
+ChangePinName(IF1, 7, 7)
+ChangePinName(IF1, 6, 6)
+ChangePinName(IF1, 5, 5)
+ChangePinName(IF1, 4, 4)
+ChangePinName(IF1, 3, 3)
+ChangePinName(IF1, 2, 2)
+ChangePinName(IF1, 1, 1)
+
+# Start of element CTRL1
+ChangePinName(CTRL1, 9, 9)
+ChangePinName(CTRL1, 8, 8)
+ChangePinName(CTRL1, 7, 7)
+ChangePinName(CTRL1, 6, 6)
+ChangePinName(CTRL1, 5, 5)
+ChangePinName(CTRL1, 4, 4)
+ChangePinName(CTRL1, 3, 3)
+ChangePinName(CTRL1, 2, 2)
+ChangePinName(CTRL1, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element Q1
+ChangePinName(Q1, 2, collector)
+ChangePinName(Q1, 3, emitter)
+ChangePinName(Q1, 1, base)
---
+++ gedasymbols/footprints/VISHAY_T18.fp
@@ -0,0 +1,15 @@
+Element["" "VISHAY_T18" "VISHAY_T18" "VISHAY_T18" 1500.00mil 500.00mil -150.00mil -200.00mil 0 100 ""]
+(
+	Attribute("gedasymbols::url" "http://www.gedasymbols.org/user/kai_martin_knaak/footprints/potentiometer/VISHAY_T18.fp")
+	Pin [0.0000 0.0000 72.00mil 39.37mil 75.94mil 30.00mil "1" "1" "square"]
+	Pin [0.0000 500.00mil 72.00mil 39.37mil 75.94mil 30.00mil "3" "3" ""]
+	Pin [-100.00mil 300.00mil 72.00mil 39.37mil 75.94mil 30.00mil "2" "2" ""]
+	ElementLine [-5.00mil 685.00mil -5.00mil 625.00mil 25.00mil]
+	ElementLine [-100.00mil 685.00mil -5.00mil 685.00mil 25.00mil]
+	ElementLine [-100.00mil 625.00mil -100.00mil 685.00mil 25.00mil]
+	ElementLine [-150.00mil 625.00mil -150.00mil -125.00mil 25.00mil]
+	ElementLine [50.00mil 625.00mil -150.00mil 625.00mil 25.00mil]
+	ElementLine [50.00mil -125.00mil 50.00mil 625.00mil 25.00mil]
+	ElementLine [-150.00mil -125.00mil 50.00mil -125.00mil 25.00mil]
+
+	)
