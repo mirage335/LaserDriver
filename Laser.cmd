9a6d2d6bb1cd8dc5996d7801b056f14bf11c4f2a
mirage335 <spamfreemirage335@gmail.com>
2015-11-15 00:47:56 -0500

    Provisions for pulsed mode operation.

---
+++ Laser.cmd
@@ -0,0 +1,24 @@
+# Pin name action command file
+
+# Start of element D1
+ChangePinName(D1, 1, 1)
+ChangePinName(D1, 2, 2)
+
+# Start of element FAN1
+ChangePinName(FAN1, 2, 2)
+ChangePinName(FAN1, 1, 1)
+
+# Start of element L1
+ChangePinName(L1, 2, K)
+ChangePinName(L1, 1, A)
+
+# Start of element LASER1
+ChangePinName(LASER1, 9, 9)
+ChangePinName(LASER1, 8, 8)
+ChangePinName(LASER1, 7, 7)
+ChangePinName(LASER1, 6, 6)
+ChangePinName(LASER1, 5, 5)
+ChangePinName(LASER1, 4, 4)
+ChangePinName(LASER1, 3, 3)
+ChangePinName(LASER1, 2, 2)
+ChangePinName(LASER1, 1, 1)
